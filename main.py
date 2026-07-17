170ec853fc5c2661ec79656f20b79a2c947b04e9
Ryoma Morita <p1ha1czx@s.okayama-u.ac.jp>
2026-07-17 20:33:24 +0900

    feat: refactor main function to read multiple Fortran files and save to CSV

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import glob
 import numpy as np
+import pandas as pd
 from scipy.io import FortranFile
 
 def main():
-    file_name = "data/4096/keta2/s2_64.float"
-    with FortranFile(file_name, 'r') as f:
-        # Read the data
-        data = f.read_reals(dtype='float32')
+    i = np.tile(np.arange(1, 65), 64 * 64)
+    j = np.repeat(np.tile(np.arange(1, 65), 64), 64)
+    k = np.repeat(np.arange(1, 65), 64 * 64)
 
-    print(np.shape(data))
-    print(data)
+    data = np.vstack([i, j, k])
+
+    df = pd.DataFrame(data, columns=['i', 'j', 'k'])
+    
+    file_pattern = "data/4096/keta2/*.float"
+    file_list = glob.glob(file_pattern)
+
+    for file_name in file_list:
+        with FortranFile(file_name, 'r') as f:
+            df = pd.concat([df, pd.DataFrame(f.read_reals(dtype='float32'), columns=[file_name])], ignore_index=True)
+
+    df.to_csv("test.csv", index=False)
 
 if __name__ == "__main__":
     main()
