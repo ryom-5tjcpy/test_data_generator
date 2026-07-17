b5f5c64d57303f2ca95787c3e5ca99d9aa6a8fc1
Ryoma Morita <p1ha1czx@s.okayama-u.ac.jp>
2026-07-17 16:23:16 +0900

    feat: implement data reading from Fortran file

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from test-data-generator!")
+import numpy as np
+from scipy.io import FortranFile
 
+def main():
+    file_name = "data/4096/keta2/s2_64.float"
+    with FortranFile(file_name, 'r') as f:
+        # Read the data
+        data = f.read_reals(dtype='float32')
+
+    print(np.shape(data))
+    print(data)
 
 if __name__ == "__main__":
     main()
